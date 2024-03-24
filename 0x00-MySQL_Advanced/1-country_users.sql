7dfe3107cd253c67bc48f4aa72e515807a9a7979
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-03-24 15:35:01 +0100

    a SQL script that creates a table users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,13 +1,8 @@
 -- script to create a table for users
--- id, email, name
--- id is a primary key
--- email is unique
--- name is a string
--- country is an enum with values US, CO, TN
-
-CREATE TABLE IF NOT EXISTS users (
+-- id, email, name, country(enumeration of US, CO and TN)
+CREATE TABLE IF NOT EXISTS users(
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
-    country enum('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
