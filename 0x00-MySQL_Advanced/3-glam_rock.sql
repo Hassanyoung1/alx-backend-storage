dd0a1886a61fddf5efb7fa794c1f25f8161ac7a6
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-03-25 08:21:50 +0100

    a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,4 @@
 -- use attributes formed and split for computing the lifespan
 
 SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM
-metal_bands WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
