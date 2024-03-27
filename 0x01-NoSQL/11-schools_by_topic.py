9e6149ea5fd296b25756ca2ebaf44271983f7499
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-03-27 16:25:47 +0100

    a Python function that returns the list of school having a specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 """
-a Python function that returns the list of a specific topic
+change school topic
 """
 import pymongo
 
 
-def schools_by_topic(mongo_collection, topic):
-    return mongo_collection.find({"topics": topic})
+def update_topics(mongo_collection, name, topics):
+    """
+    update many rows
+    """
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
