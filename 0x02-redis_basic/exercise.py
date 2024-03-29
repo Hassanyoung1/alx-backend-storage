db3e1c8663b705e60a9ec1a60a6babde810351e2
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-03-29 19:17:36 +0100

    a

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 """
-a Cache class. In the __init__ method, store an instance of the Redis client as a private variable named _redis (using redis.Redis()) and flush the instance using flushdb.
+a Cache class. In the __init__ method, store an instance of the
+Redis client as a private variable named _redis
+(using redis.Redis()) and flush the instance using flushdb.
 """
 
 import redis
