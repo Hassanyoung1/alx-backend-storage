d953bc1db0829292be9a71968caff40d8a6bb6c4
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-03-29 19:04:53 +0100

    a

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+a Cache class. In the __init__ method, store an instance of the Redis client as a private variable named _redis (using redis.Redis()) and flush the instance using flushdb.
+"""
+
+import redis
+from uuid import uuid4
+
+
+class Cache:
+    """
+    A class to store data in Redis
+    """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    """
+    A method to store data in Redis
+    """
+    def store(self, data: str):
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
